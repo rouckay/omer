e149b8a5d847575d0a0330045645fd3d2de6496b
Farhad Rahmani <55535348+2019farhad@users.noreply.github.com>
2022-04-11 22:00:38 +0430

    Add To Database

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const result = document.querySelector('#result');
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(show);
-    } else {
-        alert("Geolocation is not supported by this browser.")
-    }
-}
+// const result = document.querySelector('#result');
 
-function show(position) {
-    result.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-}
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(show);
+
+//     } else {
+//         alert("Geolocation is not supported by this browser.")
+//     }
+// }
+
+// function show(position) {
+//     result.innerHTML = "Latitude: " + position.coords.latitude +
+//         "<br>Longitude: " + position.coords.longitude;
+// }
+// const findperson = () => {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(finded)
+//     }
+// }
+// function finded(savingLoc) {
+//     var latitudeForSave = savingLoc.coords.latitude
+//     var longitudeForSave = savingLoc.coords.longitude
+//     firebase.database().ref('person/' + 1).set({
+//         latitude: latitudeForSave,
+//         longitude: longitudeForSave
+//     })
+// }
+
